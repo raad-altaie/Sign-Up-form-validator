3c61357b1436a6270ca5ae0f3f6945ee9da99059
Raad Altaie <raad.altaie@icloud.com>
2017-07-23 08:40:36 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,96 +1,76 @@
- //  Sign Up form
+//  Sign Up form
+var firstName = document.getElementById('signupname1');
+var lastName = document.getElementById('signupname2');
+var email = document.getElementById('signupemail');
+var password = document.getElementById('signuppw');
+var passwordConfirm = document.getElementById('signuppw_confirm');
+var phoneNumber = document.getElementById('signupphone');
+var checkBox = document.getElementById('checkBox');
+var submit = document.getElementById("validate");
 
-function validate() {
-    var name1 = document.getElementById('signupname1').value;
-    var name2 = document.getElementById('signupname2').value;
-    var email = document.getElementById('signupemail').value;
-    var password = document.getElementById('signuppw').value;
-    var password_confirm = document.getElementById('signuppw_confirm').value;
-    var phonenumber = document.getElementById('signupphone').value;
+submit.addEventListener("click", function () {
+
     var email_patt = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     var password_patt = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,32}$/;
     var phone_patt = /^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/;
-    var email_match =email.match(email_patt);
-    var password_match =password.match(password_patt);
-    var phone_match =phonenumber.match(phone_patt);
+    var email_match = email.value.match(email_patt);
+    var password_match = password.value.match(password_patt);
+    var phone_match = phoneNumber.value.match(phone_patt);
 
-    if  (name1 == "") {  
-        document.getElementById('signupname1').setCustomValidity('Please submit your First Name');
-        document.getElementById('signupname1').style.borderColor="red";
+    if (!firstName.value) {
+        firstName.setCustomValidity('Please submit your First Name');
+        firstName.style.borderColor = "red";
         return false;
-    }
-    else if (name2 == "") {  
-        document.getElementById('signupname2').setCustomValidity('Please submit your Last Name');
-        document.getElementById('signupname2').style.borderColor="red";
+    } else if (!lastName.value) {
+        lastName.setCustomValidity('Please submit your Last Name');
+        lastName.style.borderColor = "red";
         return false;
-    }
-
-    else if  (email == "") {  
-        document.getElementById('signupemail').setCustomValidity('Please submit your Email');
-        document.getElementById('signupemail').style.borderColor="red";
+    } else if (!email.value) {
+        email.setCustomValidity('Please submit your Email');
+        email.style.borderColor = "red";
         return false;
-    }
-
-    else if (email_match == null) { 
-        document.getElementById('signupemail').setCustomValidity('Please submit a vaild Email');
-        document.getElementById('signupemail').style.borderColor="red";
-        return false;                 
-        }
-
-    else if (password == "") {  
-        document.getElementById('signuppw').setCustomValidity('Please enter a password');
-        document.getElementById('signuppw').style.borderColor="red";
+    } else if (email_match == null) {
+        email.setCustomValidity('Please submit a vaild Email');
+        email.style.borderColor = "red";
         return false;
-    }
-    else if (password_match == null) { 
-        document.getElementById('signuppw').setCustomValidity('The password must be between (8 to 32) characters including Uppercase letter, Special character and Alphanumeric characters.');
-        document.getElementById('signuppw').style.borderColor="red";
-        return false;  
-    }
-    else if (password_confirm == "") {  
-        document.getElementById('signuppw_confirm').setCustomValidity('Please confirm your password');
-        document.getElementById('signuppw_confirm').style.borderColor="red";
+    } else if (!password.value) {
+        password.setCustomValidity('Please enter a password');
+        password.style.borderColor = "red";
         return false;
-        }
-    else if (password_confirm != password) {  
-        document.getElementById('signuppw_confirm').setCustomValidity('Password Must be Matching');
-        document.getElementById('signuppw_confirm').style.borderColor="red";
+    } else if (password_match == null) {
+        password.setCustomValidity('The password must be between (8 to 32) characters including Uppercase letter, Special character and Alphanumeric characters.');
+        password.style.borderColor = "red";
         return false;
-    }
-    else if ((phone_match == null) || (phonenumber == "")) { 
-        document.getElementById('signupphone').setCustomValidity('Please submit a vaild phone number');
-        document.getElementById('signupphone').style.borderColor="red";
-        return false;                 
-    }
-    else if (document.getElementById('checkbx').checked == false){
-        document.getElementById('checkbx').setCustomValidity('you must agree to the terms of service.');
-    }
-    else {
+    } else if (passwordConfirm.value == "") {
+        passwordConfirm.setCustomValidity('Please confirm your password');
+        passwordConfirm.style.borderColor = "red";
+        return false;
+    } else if (passwordConfirm.value !== password.value) {
+        passwordConfirm.setCustomValidity('Password Must be Matching');
+        passwordConfirm.style.borderColor = "red";
+        return false;
+    } else if ((phone_match == null) || (!phoneNumber.value)) {
+        phoneNumber.setCustomValidity('Please submit a vaild phone number');
+        phoneNumber.style.borderColor = "red";
+        return false;
+    } else if (checkBox.checked == false) {
+        checkBox.setCustomValidity('you must agree to the terms of service.');
+    } else {
         return true;
     }
-}
+});
 
+//Clearing feedback on inputs
+function Clear(Vars) {
+        for (let i = 0; i < Vars.length; i++) {
+            function Clr() {
+                Vars[i].setCustomValidity('');
+                Vars[i].style.removeProperty('border');
+            }
+            Vars[i].addEventListener("click", Clr, false);
+            Vars[i].addEventListener("oninput", Clr, false);
+        }
+    
+};
 
- // Clearing Red border for Sign Up Inputs
-
-    function ClearingEmail(){
-        document.getElementById('signupemail').setCustomValidity('');
-        document.getElementById('signupemail').style.removeProperty('border');
-        document.getElementById('signupname1').setCustomValidity('');
-        document.getElementById('signupname1').style.removeProperty('border');
-        document.getElementById('signupname2').setCustomValidity('');
-        document.getElementById('signupname2').style.removeProperty('border');
-    }
-    function ClearingPassword(){
-        document.getElementById('signuppw').setCustomValidity('');
-        document.getElementById('signuppw').style.removeProperty('border');
-        document.getElementById('signuppw_confirm').setCustomValidity('');
-        document.getElementById('signuppw_confirm').style.removeProperty('border');
-    }
-    function ClearingPhone(){
-        document.getElementById('signupphone').setCustomValidity('');
-        document.getElementById('signupphone').style.removeProperty('border');
-    }
-    function ClearingCheckbox(){
-        document.getElementById('checkbx').setCustomValidity('');
-    }
+Clear([email, firstName, lastName, password, passwordConfirm, phoneNumber, checkBox]);
